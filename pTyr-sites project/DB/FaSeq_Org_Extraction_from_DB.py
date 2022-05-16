0d77e2958f103cb9983482628fd7dea34a6b727b
tviguier <97729512+tviguier@users.noreply.github.com>
2022-05-16 13:25:05 +0200

    v2
    Add csv dataframe

--- pTyr-sites project/DB/FaSeq_Org_Extraction_from_DB.py
+++ pTyr-sites project/DB/FaSeq_Org_Extraction_from_DB.py
@@ -1,4 +1,5 @@
 from Bio import SeqIO
+import pandas as pd
 import time 
 import os 
 
@@ -20,7 +21,6 @@ print(" - FILE NAME : ", FaSeq_DB_NAME)
 print(f" - FORMAT : {FaSeq_DB_FORMAT}")
 print(f" - NUMBER OF FASTA SEQUENCES DETECTED : {len(FaSeq_DB_PARSE)}")
 print(f" - ORGANISM SELETED : {ORGANISM}")
-print("")
 
 print("")
 print("###########################################################################################")
@@ -30,25 +30,29 @@ print("")
 
 Stime = time.time()
 
-OUTPUT_FORMAT = ".fa"
-OUTPUT_NAME = f"FaSeq_DB_{ORGANISM.replace(' ', '_')}"
-OUTPUT_FILE = f"{OUTPUT_NAME}{OUTPUT_FORMAT}"
+OUTPUT_FASTA_FORMAT = ".fa"
+OUTPUT_CSV_FORMAT = ".csv"
+
+OUTPUT_FASTA_NAME = f"FaSeq_DB_{ORGANISM.replace(' ', '_')}"
+OUTPUT_FASTA_FILE = f"{OUTPUT_NAME}{OUTPUT_FASTA_FORMAT}"
 
 OUTPUT = open(OUTPUT_FILE, "w")
 
 ORGANISM_mod = "[" + ORGANISM + "]"
+
+df_DB_FILE = pd.DataFrame()
 
 i = 0
 COUNT = 0
 
 while i < len(FaSeq_DB_PARSE) : 
     
-    DESCRIPTION = FaSeq_DB_PARSE[i].description
+    DESCRIPTION = str(FaSeq_DB_PARSE[i].description)
     FASTA_SEQUENCE = str(FaSeq_DB_PARSE[i].seq)
     
     Homo_DESCRIPTION = (DESCRIPTION.split(ORGANISM_mod)[0]).split("\x01")[-1]
     Homo_ID = Homo_DESCRIPTION.split(" ")[0]
-    Homo_ID_VERSION = Homo_ID.split(".")[-1]
+    Homo_ID_VERSION = int(Homo_ID.split(".")[-1])
     Homo_ID_NAME = (Homo_DESCRIPTION.split(";")[0]).split("RecName: Full=")[-1]
     
     if DESCRIPTION.find(ORGANISM_mod) != -1:
@@ -56,25 +60,41 @@ while i < len(FaSeq_DB_PARSE) :
         OUTPUT.write(Homo_DESCRIPTION + "\n")
         OUTPUT.write(FASTA_SEQUENCE + "\n" + "\n")
         
+        df_ROW = pd.DataFrame({'ID' : [Homo_ID],
+                               'ID_VERSION' : [Homo_ID_VERSION],
+                               'ID_NAME' : [Homo_ID_NAME],
+                               'DESCRIPTION' : [Homo_DESCRIPTION],
+                               'LEN_SEQ' : [len(FASTA_SEQUENCE)],
+                               'SEQUENCE' : [FASTA_SEQUENCE]})
+        
+        
+        df_DB_FILE = pd.concat([df_DB_FILE, df_ROW])
+           
         COUNT += 1
         
     i += 1
 
+df_DB_FILE = df_DB_FILE.reset_index(drop=True)    
+
 OUTPUT.close()    
 
-NEW_OUTPUT_NAME = f"{OUTPUT_NAME}_{COUNT}"
-NEW_OUTPUT_FILE = f"{NEW_OUTPUT_NAME}{OUTPUT_FORMAT}"
+NEW_OUTPUT_FASTA_NAME = f"{OUTPUT_FASTA_NAME}_{COUNT}"
+NEW_OUTPUT_FASTA_FILE = f"{NEW_OUTPUT_FASTA_NAME}{OUTPUT_FASTA_FORMAT}"
+
+OUTPUT_CSV_FILE = f"{NEW_OUTPUT_FASTA_NAME}{OUTPUT_CSV_FORMAT}"
+
+df_DB_FILE.to_csv(CSV_OUTPUT_FILE, sep='\t', mode='a', index=False)
 
 Etime = time.time()
 Rtime = Etime - Stime
 
-os.rename(OUTPUT_FILE, NEW_OUTPUT_FILE)
+os.rename(OUTPUT_FASTA_FILE, NEW_OUTPUT_FASTA_FILE)
 
 print("FASTA SEQUENCES EXTRACTION DONE")
 print("")
 print(f" - {COUNT} FASTA SEQUENCES EXTRACTED")
 print(f" - RUN TIME : {round(Rtime, 3)}")
-print(f" - '{NEW_OUTPUT_FILE}' CREATED")
-
+print(f" - '{NEW_OUTPUT_FASTA_FILE}' CREATED")
+print(f" - '{OUTPUT_CSV_FILE}' CREATED")
 print("")
 print("DONE")
