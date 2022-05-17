a55f408a97e88750fcb8edd4d4788a61fb52145c
tviguier <97729512+tviguier@users.noreply.github.com>
2022-05-17 22:12:02 +0200

    Update OS_Extraction_from_UniprotKB_db.py

--- py_scripts/OS_Extraction_from_UniprotKB_db.py
+++ py_scripts/OS_Extraction_from_UniprotKB_db.py
@@ -3,27 +3,28 @@ import pandas as pd
 import time 
 import os 
 
-FaSeq_DB = input("INPUT DATABASE FILE TO PARSE : ")
-#FaSeq_DB = "UniprotKB_2022_01.fa"
+DataBase = input("INPUT FASTA DATABASE FILE TO PARSE : ")
+#DataBase = "UniprotKB_2022_01.fa"
+
+print("")
 
 OS = input("INPUT TYPE OF ORGANISM WANTED (Homo sapiens) : ")
 #OS = "Homo sapiens"
 
-list_FaSeq_DB = list(SeqIO.parse(FaSeq_DB, "fasta"))
-
-FaSeq_DB_FORMAT = f".{FaSeq_DB.split('.')[-1]}"
-FaSeq_DB_NAME = FaSeq_DB.replace(f'{FaSeq_DB_FORMAT}', '')
+list_DataBase = list(SeqIO.parse(DataBase, "fasta"))
+DataBase_FORMAT = f".{DataBase.split('.')[-1]}"
+DataBase_NAME = DataBase.replace(f'{DataBase_FORMAT}', '')
 
 print("")
-print(" - FILE NAME : ", FaSeq_DB_NAME)
-print(f" - FORMAT : {FaSeq_DB_FORMAT}")
-print(f" - NUMBER OF FASTA SEQUENCES DETECTED : {len(list_FaSeq_DB)}")
+print("")
+print(" - FILE NAME : ", DataBase_NAME)
+print(f" - FORMAT : {DataBase_FORMAT}")
+print(f" - NUMBER OF FASTA SEQUENCES DETECTED : {len(list_DataBase)}")
 print(f" - ORGANISM SELETED : {OS}")
 print("")
-
 print("")
 print("###########################################################################################")
-print(f"##### {OS} FASTA SEQUENCES EXTRACTION FROM {FaSeq_DB}")
+print(f"##### {OS} FASTA SEQUENCES EXTRACTION FROM {DataBase}")
 print("###########################################################################################")
 print("")
 
@@ -34,7 +35,7 @@ OUTPUT_CSV_FORMAT = ".csv"
 
 OS_RESTRUC = "OS=" + OS + " "
 
-OUTPUT_NAME = f"{FaSeq_DB_NAME}_{OS.replace(' ', '_')}"
+OUTPUT_NAME = f"{DataBase_NAME}_{OS.replace(' ', '_')}"
 
 FASTA_FILE = open(OUTPUT_NAME, "w")
 
@@ -43,10 +44,10 @@ COUNT = 0
 
 df_FaSeq_OS_FILE = pd.DataFrame()
 
-while i < len(list_FaSeq_DB) : 
+while i < len(list_DataBase) : 
     
-    FaSeq_DESCRIPTION = list_FaSeq_DB[i].description
-    FaSeq_SEQUENCE = str(list_FaSeq_DB[i].seq)
+    FaSeq_DESCRIPTION = list_DataBase[i].description
+    FaSeq_SEQUENCE = str(list_DataBase[i].seq)
     
     OS_ACCESSION_ID = FaSeq_DESCRIPTION.split("|")[1]
     OS_ACCESSION_NAME = (FaSeq_DESCRIPTION.split("|")[2]).split(" ")[0]
@@ -54,7 +55,7 @@ while i < len(list_FaSeq_DB) :
     
     if FaSeq_DESCRIPTION.find(OS_RESTRUC) != -1 :
                       
-        # df_OS_ ROW : ROW WITH ALL THE INFORMATIONS CONTAINS IN THE FASTA SEQUENCE
+        # df_ROW : ROW WITH ALL THE INFORMATIONS CONTAINS IN THE FASTA SEQUENCE
         df_OS_ROW = pd.DataFrame({'ACCESSION_ID' : [OS_ACCESSION_ID],
                                   'ACCESSION_NAME' : [OS_ACCESSION_NAME],
                                   'DESCRIPTION' : [OS_DESCRIPTION],
@@ -71,7 +72,7 @@ while i < len(list_FaSeq_DB) :
 
 df_FaSeq_OS_FILE = df_FaSeq_OS_FILE.reset_index(drop=True)
 
-OUTPUT_FILE.close()    
+FASTA_FILE.close()    
 
 NEW_OUTPUT_FASTA_NAME = f"{OUTPUT_NAME}-{COUNT}"
 OUTPUT_FASTA_FILE = f"{NEW_OUTPUT_FASTA_NAME}{OUTPUT_FASTA_FORMAT}"
@@ -93,4 +94,4 @@ print("")
 print(f" - '{OUTPUT_FASTA_FILE}' CREATED")
 print(f" - '{OUTPUT_CSV_FILE}' CREATED")
 print("")
-print("DONE")
+print("END")
